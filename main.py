b9905491323b73f8ba42cafb4c9dc31c15dc618f
isaacvr1235 <isaacvr1235@gmail.com>
2026-03-27 12:46:45 -0600

    actualizar ejecución en main

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from pipeline.pipeline import Pipeline
 from data.data import DataLoader
-from transformers.transformers import LimpiadorNulos, Scaler
+from transformers.transformers import ProcesadorDatos
 from models.models import ModeloRegresionLineal
 from evaluation.metrics import Metricas
 
-
 class Main:
-
     def main(self):
-
         loader = DataLoader()
-        dataset = loader.cargar("datos.csv")
+        dataset = loader.cargar("estudiantes (1).csv")
 
         pipeline = Pipeline()
-        pipeline.agregar_paso(LimpiadorNulos())
-        pipeline.agregar_paso(Scaler())
+        
+        paso_limpieza = ProcesadorDatos(accion='limpieza', metodo=2, columnas=['Edad', 'Promedio'])
+        pipeline.agregar_paso(paso_limpieza)
+
+        paso_escalado = ProcesadorDatos(accion='scaler', metodo=1, columnas=['Edad', 'Promedio'])
+        pipeline.agregar_paso(paso_escalado)
 
         modelo = ModeloRegresionLineal()
         metricas = Metricas()
@@ -23,7 +24,6 @@ class Main:
         pipeline.metricas = metricas
 
         pipeline.ejecutar(dataset)
-
 
 if _name_ == "_main_":
     Main().main()
