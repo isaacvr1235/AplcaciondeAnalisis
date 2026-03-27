09f758c9a5bd0ab70a1245891de5a8a7dea101f1
isaacvr1235 <isaacvr1235@gmail.com>
2026-03-27 13:34:32 -0600

    implementar DataLoader, Dataset y prueba de ingesta de CSV

--- data/data.py
+++ data/data.py
@@ -1,54 +1,29 @@
+import pandas as pd
+import os
+
 class Dataset:
+    def __init__(self, data: pd.DataFrame, nombre_fuente: str):
+        self.data = data
+        self.nombre_fuente = nombre_fuente
+        self.n_filas, self.n_columnas = data.shape
 
-    def _init_(self, nombre=None, datos=None, origen=None, fecha_carga=None, metadata=None):
-        self.nombre = nombre
-        self.datos = datos
-        self.origen = origen
-        self.fecha_carga = fecha_carga
-        self.metadata = metadata
-
-    def get_dimensiones(self):
-        return None
-
-    def get_columnas(self):
-        return None
-
-    def get_datos(self):
-        return self.datos
-
-    def info(self):
-        pass
-
-    def head(self, n):
-        return None
-
-    def guardar(self, ruta):
-        pass
-
+    def head(self, n=5):
+        return self.data.head(n)
 
 class DataLoader:
-
-    def cargar(self, ruta):
+    def __init__(self):
         pass
 
-
-class DatasetSplit:
-
-    def _init_(self, train, test):
-        self.train = train
-        self.test = test
-
-
-class DataSplitter:
-
-    def _init_(self, test_size=0.2, random_state=42):
-        self.test_size = test_size
-        self.random_state = random_state
-
-    def split(self, dataset):
-
-        train = dataset
-        test = dataset
-
-        return DatasetSplit(train, test)
-    
+    def cargar_csv(self, ruta_archivo: str) -> Dataset:
+        if not os.path.exists(ruta_archivo):
+            raise FileNotFoundError(f"Error: El archivo {ruta_archivo} no existe.")
+        
+        try:
+            df = pd.read_csv(ruta_archivo)
+            nombre_archivo = os.path.basename(ruta_archivo)
+            
+            print(f"Exito: Archivo '{nombre_archivo}' cargado correctamente.")
+            return Dataset(data=df, nombre_fuente=nombre_archivo)
+            
+        except Exception as e:
+            raise RuntimeError(f"Ocurrió un error al leer el archivo CSV: {e}")
